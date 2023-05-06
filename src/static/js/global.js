0526a500046d36b026ed14c2847278a391bcefa4
rm82966@estudante.fieb.edu.br <rm82966@estudante.fieb.edu.br>
2023-05-06 14:28:44 -0300

    Testando

---
+++ src/static/js/global.js
@@ -0,0 +1,27 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+
+    if (hr < 10) hr = '0' + hr;
+
+    if (min < 10) min = '0' + min;
+
+    if (s < 10) s = '0' + s;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = s;
+
+    date = new Date();
+year = date.getFullYear();
+month = date.getMonth() + 1;
+day = date.getDate();
+document.getElementById("current_date").innerHTML = month + "/" + day + "/" + year;
+
+})
--- src/static/js/login.js
+++ src/static/js/login.js
@@ -1,33 +1,42 @@
-const form = document.querySelector('form');
-const forgotPasswordLink = document.querySelector('.forgot-password');
-const forgotPasswordForm = document.querySelector('.forgot-password-form');
-const backToLoginLink = document.querySelector('.back-to-login');
+const registerBtn = document.getElementById("register-link");
+const backToLoginBtn = document.querySelectorAll(".back-to-login");
+const forgotPasswordBtn = document.querySelector(".forgot-password");
+const forgotPasswordForm = document.querySelector(".forgot-password-form");
+const loginForm = document.querySelector("form");
+const registerForm = document.querySelector(".register-form");
+const form = document.getElementById("login-form");
 
-// mostra o formulário de "esqueci a senha" quando o link é clicado
-forgotPasswordLink.addEventListener('click', e => {
+// Esconde os formulários de registro e recuperação de senha
+registerForm.style.display = "none";
+forgotPasswordForm.style.display = "none";
+
+// Adiciona o evento de clique para mostrar o formulário de registro
+registerBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  form.style.display = 'none';
-  forgotPasswordForm.style.display = 'block';
+  loginForm.style.display = "none";
+  registerForm.style.display = "block";
 });
 
-// volta para o formulário de login quando o link é clicado
-backToLoginLink.addEventListener('click', e => {
-  e.preventDefault();
-  form.style.display = 'block';
-  forgotPasswordForm.style.display = 'none';
+// Adiciona o evento de clique para voltar ao formulário de login
+backToLoginBtn.forEach(function (button) {
+  button.addEventListener("click", function (e) {
+    e.preventDefault();
+    loginForm.style.display = "block";
+    registerForm.style.display = "none";
+    forgotPasswordForm.style.display = "none";
+  });
 });
 
-// envia um email para o endereço fornecido no formulário de "esqueci a senha"
-forgotPasswordForm.addEventListener('submit', e => {
+// Adiciona o evento de clique para mostrar o formulário de recuperação de senha
+forgotPasswordBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  const email = document.getElementById('forgot-password-email').value;
-  // aqui você pode adicionar a lógica para enviar o email com o link para redefinir a senha
-  // depois de enviar o email, você pode exibir uma mensagem para o usuário informando que o link foi enviado com sucesso
+  loginForm.style.display = "none";
+  forgotPasswordForm.style.display = "block";
 });
 
 // lista de usuários cadastrados
 const users = [
-  { username: 'admin', password: '123456', type: 'adm', name: 'Conta adm teste' },
+  { username: 'Juliane', password: '123456', type: 'adm', name: 'Juliane' },
   { username: 'funcionario', password: '123456', type: 'funcionario', name: 'Conta funcionario teste' },
 ];
 
@@ -35,15 +44,43 @@ function setStorage(login){
   localStorage.setItem('login', JSON.stringify(login));
 }
 
-form.addEventListener('submit', (event) => {
+// Mostra o formulário de "esqueci a senha" quando o link é clicado
+$('.forgot-password').click(function(e) {
+  e.preventDefault();
+  $('form').hide();
+  $('.forgot-password-form').show();
+});
+
+// Volta para o formulário de login quando o link é clicado
+$('.back-to-login').click(function(e) {
+  e.preventDefault();
+  $('form').show();
+  $('.forgot-password-form').hide();
+});
+
+// Envia um email para o endereço fornecido no formulário de "esqueci a senha"
+$('.forgot-password-form').submit(function(e) {
+  e.preventDefault();
+  const email = $('#forgot-password-email').val();
+  // Aqui você pode adicionar a lógica para enviar o email com o link para redefinir a senha
+  // Depois de enviar o email, você pode exibir uma mensagem para o usuário informando que o link foi enviado com sucesso
+});
+
+// Trata o envio do formulário de login
+$('form').submit(function(event) {
   event.preventDefault();
 
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
+  const username = $('#username').val();
+  const password = $('#password').val();
 
-  // busca pelo usuário na lista de usuários cadastrados
-  const user = users.find((u) => u.username === username && u.password === password);
-
+  // Busca pelo usuário na lista de usuários cadastrados
+  let user;
+  users.forEach((u) => {
+    if (u.username === username && u.password === password) {
+      user = u;
+    }
+  });
+  
   if (user) {
     if (user.type === 'adm') {
       alert('Login realizado com sucesso como ADM!');
@@ -52,8 +89,8 @@ form.addEventListener('submit', (event) => {
       alert('Login realizado com sucesso como funcionário!');
       // window.location.href = "home.html";
     }
-    window.location.href = "home.html";
 
+    window.location.href = "home.html";
     setStorage(user);
   } else {
     alert('Nome de usuário ou senha incorretos. Tente novamente.');
